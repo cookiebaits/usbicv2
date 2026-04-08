b78fadfe5af8ce4225e0e3cdc58c62a1f22c003d
C B <cookiescambaits@gmail.com>
2026-04-08 00:50:41 -0700

    Update route.ts

--- app/api/2Fa/route.ts
+++ app/api/2Fa/route.ts
@@ -1,4 +1,5 @@
 // app/api/2Fa/route.ts
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from "next/server";
 import dbConnect from "@/lib/mongodb";
 import User from "@/models/User";
